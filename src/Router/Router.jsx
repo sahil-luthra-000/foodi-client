d354854313e344c23c0cc6e32ceadf956d52df06
Sahil <145024652+sahil-luthra-000@users.noreply.github.com>
2024-09-22 02:23:34 +0530

    Update Router.jsx

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -81,7 +81,7 @@ const router = createBrowserRouter([
       {
         path: "update-menu/:id",
         element: <UpdateMenu />,
-        loader: ({ params }) => fetch(`http://localhost:6001/menu/${params.id}`),
+        loader: ({ params }) => fetch(`https://foodi-rnmx.onrender.com/menu/${params.id}`),
       },
     ],
   },
