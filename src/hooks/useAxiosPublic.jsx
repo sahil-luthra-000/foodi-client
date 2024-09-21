efcced43fbb2a88585ec830eb1a76c5d7aaa6206
Sahil <145024652+sahil-luthra-000@users.noreply.github.com>
2024-09-22 02:19:58 +0530

    Update useAxiosPublic.jsx

--- src/hooks/useAxiosPublic.jsx
+++ src/hooks/useAxiosPublic.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 
 const axiosPublic =  axios.create({
-    baseURL: 'http://localhost:6001',
+    baseURL: 'https://foodi-rnmx.onrender.com',
   })
 
 const useAxiosPublic = () => {
