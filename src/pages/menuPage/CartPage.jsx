cc9f527c3d77799f1eea216f4ad78ae9dda45880
Sahil <145024652+sahil-luthra-000@users.noreply.github.com>
2024-09-23 00:19:59 +0530

    Update CartPage.jsx

--- src/pages/menuPage/CartPage.jsx
+++ src/pages/menuPage/CartPage.jsx
@@ -20,7 +20,7 @@ const CartPage = () => {
   // Handle quantity increase
   const handleIncrease = async (item) => {
     try {
-      const response = await fetch(`https://foodi-rnmx.onrender.com/carts/${item._id}`, {
+      const response = await fetch(`https://foodi-server-ixn9.onrender.com/carts/${item._id}`, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
@@ -104,7 +104,7 @@ const CartPage = () => {
       confirmButtonText: "Yes, delete it!",
     }).then((result) => {
       if (result.isConfirmed) {
-        axios.delete(`https://foodi-rnmx.onrender.com/carts/${item._id}`).then(response => {
+        axios.delete(`https://foodi-server-ixn9.onrender.com/carts/${item._id}`).then(response => {
           if (response) {
             refetch();
              Swal.fire("Deleted!", "Your file has been deleted.", "success");
